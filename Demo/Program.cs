005204689038f1a15815c62ef7b37997b4a58079
entsarahmed <entesara500@gmail.com>
2024-06-21 12:39:24 +0300

    String Formatting

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -352,6 +352,33 @@
 
 
             #endregion
+
+
+            #region String Formatting
+
+            ////  Equation: 3 + 4 = 8
+
+            //int X = 3, Y = 4;
+
+            //string Message = "Equation: " + X + " + " + Y + " = " + (X + Y);
+
+            //// 1.  Composite Formatting 
+
+            //Message = string.Format("Equation: {0} + {1} = {2}",X,Y,(X,Y));
+
+
+            ////2. String Interpolation
+            ////$ : Manpulation Operator
+
+            ////Message = `Equation: ${X} + ${Y} = ${X+Y}`=>JS
+
+            //Message = $"Equation: {X} + {Y} = {X+Y}";
+
+            
+            //Console.WriteLine(Message);
+
+
+            #endregion
         }
     }
 }
